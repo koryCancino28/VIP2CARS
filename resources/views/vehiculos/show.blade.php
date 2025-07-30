09030096917eba229d06bcc1b2ba538a6d7dcf6a
kory <koryna654@gmail.com>
2025-07-30 15:37:24 -0500

    ..

--- resources/views/vehiculos/show.blade.php
+++ resources/views/vehiculos/show.blade.php
@@ -56,7 +56,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="fas fa-times"></i> Cerrar</button>
             </div>
         </div>
     </div>
